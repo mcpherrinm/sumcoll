eb905cc0e0d3cf3e8ba822fbb16039abb4a74bc3
Matthew McPherrin <git@mcpherrin.ca>
2019-10-27 15:56:01 -0700

    Add a python implementation of the BSD sum tool

---
+++ sum.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+### Sum.py
+### An implementation of the BSD `sum` checksum in python.
+
+import math, sys
+from typing import Iterable, Tuple
+
+def rotate_right_16bit(data: int) -> int:
+  """rotate right a 16-bit value"""
+  return (data >> 1) | ((data & 1) << 15)
+
+def compute_sum(data: bytes) -> Tuple[int, int]:
+  """
+  Compute the BSD sum checksum over data
+
+  Returns the checksum and number of 1024 byte blocks
+  """
+  sum = 0
+  for byte in data:
+    sum = rotate_right_16bit(sum) + byte
+    sum = sum & 0xffff # clamp to 16 bits
+  return sum, math.ceil(len(data) / 1024)
+
+def format_sum(sum: int, blocks: int):
+  """Format checksum and block count like sum does"""
+  return "{:05} {:5}".format(sum, blocks)
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    for filename in sys.argv[1:]:
+      try:
+        with open(filename, "rb") as fp:
+          sum, blocks = compute_sum(fp.read())
+        print(format_sum(sum, blocks), filename)
+      except Exception as e:
+        print(e)
+  else:
+    sum, blocks = compute_sum(sys.stdin.buffer.read())
+    print(format_sum(sum, blocks))
